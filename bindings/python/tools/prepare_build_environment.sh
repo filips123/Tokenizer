1c7a8a2da19dce802ede4ca7ca5c5e1681e3b93a
Filip Š <filip.stamcar@hotmail.com>
2020-03-08 21:44:59 +0100

    Fix conditions and shell on Windows

--- bindings/python/tools/prepare_build_environment.sh
+++ bindings/python/tools/prepare_build_environment.sh
@@ -12,20 +12,21 @@ PYBIND11_VERSION=${PYBIND11_VERSION:-2.4.3}
 pip install pybind11==$PYBIND11_VERSION
 
 # Install CMake on Windows and macOS
-if [[ -z "${RUNNER_OS}" ]]; then pip install "cmake==3.13.*" || pip install "cmake==3.16.*"; fi
+if [[ ! -z "${RUNNER_OS}" ]]; then pip install "cmake==3.13.*" || pip install "cmake==3.16.*"; fi
 
 # Skip double installation of libraries
 if [ -f built-$PYTHON_ARCH ]; then exit 0; fi
 touch built-$PYTHON_ARCH
 
 # Install CMake on manylinux
-if [[ ! -z "${RUNNER_OS}" ]]; then /opt/python/cp37-cp37m/bin/pip install "cmake==3.13.*" && cp /opt/python/cp37-cp37m/bin/cmake /usr/bin; fi
+if [[ -z "${RUNNER_OS}" ]]; then /opt/python/cp37-cp37m/bin/pip install "cmake==3.13.*" && cp /opt/python/cp37-cp37m/bin/cmake /usr/bin; fi
 
 # Install ICU
 curl -L -O https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/icu4c-${ICU_VERSION/./_}-src.tgz
 tar xf icu4c-*-src.tgz
 cd icu/source
 CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure --disable-shared --enable-static || true
+if [[ "${RUNNER_OS}" == "Windows" ]]; then sed -i '1 i\SHELL := /bin/sh\n' Makefile; fi
 make -j2 install
 cd $ROOT_DIR
 
@@ -35,11 +36,13 @@ tar zxf sentencepiece-${SENTENCEPIECE_VERSION}.tar.gz
 cd sentencepiece-${SENTENCEPIECE_VERSION}
 mkdir build; cd build
 cmake ..
+if [[ "${RUNNER_OS}" == "Windows" ]]; then sed -i '1 i\SHELL := /bin/sh\n' Makefile; fi
 make -j2 install
 cd $ROOT_DIR
 
 # Build Tokenizer
 mkdir build; cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DLIB_ONLY=ON -DWITH_ICU=ON ..
+if [[ "${RUNNER_OS}" == "Windows" ]]; then sed -i '1 i\SHELL := /bin/sh\n' Makefile; fi
 make -j2 install
 cd $ROOT_DIR
